df5a516a17c7915918a47a67fb5e6711470729b9
Chris Le <chris@iamchrisle.com>
2023-04-19 19:20:17 -0700

    Convert all files to use tab characters.

--- types/messages.ts
+++ types/messages.ts
@@ -2,28 +2,28 @@ import { DeviceId } from './DeviceId';
 
 
 export interface DiscoveryMessage {
-    deviceId: DeviceId;
-    source: string;
-    action: string;
-    software: {
-        name: string;
-        version: string;
-    };
-    port: number;
+	deviceId: DeviceId;
+	source: string;
+	action: string;
+	software: {
+		name: string;
+		version: string;
+	};
+	port: number;
 }
 
 export interface ConnectionInfo extends DiscoveryMessage {
-    address: IpAddress;
-    unit?: {
-        name: string,
-        type: string,
-        decks: number
-    };
+	address: IpAddress;
+	unit?: {
+		name: string,
+		type: string,
+		decks: number
+	};
 }
 
 export interface ServiceMessage<T> {
-    id: number;
-    message: T;
+	id: number;
+	message: T;
 }
 
 
